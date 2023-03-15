37c3659cd8624c7c0f1d6d3492e19de94f75f988
SergeiMart <konoplyankas@mail.ru>
2023-03-15 23:44:53 +0300

    Правки после код-ревью

--- source/js/modules/more.js
+++ source/js/modules/more.js
@@ -1,12 +1,15 @@
-const moreButton = document.querySelector('[data-more="button"]');
-const parent = document.querySelector('[data-more="parent"]');
+if (document.querySelector('[data-more="parent"]')) {
+  const moreButton = document.querySelector('[data-more="button"]');
+  const parent = document.querySelector('[data-more="parent"]');
+  parent.classList.remove('company--no-js');
 
-moreButton.addEventListener('click', function () {
-  if (parent.classList.contains('short')) {
-    parent.classList.remove('short');
-    moreButton.textContent = 'Свернуть';
-  } else {
-    parent.classList.add('short');
-    moreButton.textContent = 'Подробнее';
-  }
-});
+  moreButton.addEventListener('click', function () {
+    if (parent.classList.contains('short')) {
+      parent.classList.remove('short');
+      moreButton.textContent = 'Свернуть';
+    } else {
+      parent.classList.add('short');
+      moreButton.textContent = 'Подробнее';
+    }
+  });
+}
